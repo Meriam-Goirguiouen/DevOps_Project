2810e077c02e596d1a9a9917b9206c8e5557f1ae
Meriam-Goirguiouen <meriam.goirguiouen2002@gmail.com>
2025-04-23 09:01:04 +0100

    Ajout de UserModelTest

---
+++ tests/UserModelTest.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Tests\Models;
+use PHPUnit\Framework\TestCase;
+use App\Models\UserModel;
+  
+ class UserModelTest extends TestCase{
+    public function testDindAllUsers(){
+        $model = new UserModel();
+        $users = $model->findAll();
+
+        $this->assertIsArray($users,"findAll retourne un tableau !");
+    }
+    public function testInsertUser(){
+        $model = new UserModel();
+        $data = ['name' => 'Jamila Dahi','email' => 'jd@gk.mt'];
+        $id = $model->insert($data);
+
+        $this->assertGreaterThan(0, $id, "ID user inséré > 0");
+    }
+ }
+?>
