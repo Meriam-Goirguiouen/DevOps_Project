c238033d36283dc178692bd2ff1c9ac39c19c1e9
karimamou <moummadekarima@gmail.com>
2025-04-23 10:07:06 +0100

    modification de PanierModelTest.php

--- tests/PanierModelTest.php
+++ tests/PanierModelTest.php
@@ -28,4 +28,22 @@ class PanierModelTest extends CIUnitTestCase
 
         $this->assertGreaterThan(0, $id, "L'ID du panier inséré doit être > 0.");
     }
+
+    public function testInsertedPanierIsRetrievable()
+    {
+        $model = new PanierModel();
+
+        $data = [
+            'date_commande' => date('Y-m-d'),
+            'client' => 'Test Client',
+        ];
+
+        $id = $model->insert($data);
+
+        $panier = $model->find($id);
+
+        $this->assertNotNull($panier, "Le panier inséré doit être retrouvable.");
+        $this->assertEquals('Test Client', $panier['client'], "Le nom du client doit correspondre.");
+        $this->assertEquals(date('Y-m-d'), $panier['date_commande'], "La date de commande doit correspondre.");
+    }
 }
