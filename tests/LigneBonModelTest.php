d403c5f13dd5c373e8444fca316b72c477e50397
Meriam-Goirguiouen <meriam.goirguiouen2002@gmail.com>
2025-05-01 22:22:19 +0100

    Save Changes + add testUpdateLigneBonL test function

--- tests/LigneBonModelTest.php
+++ tests/LigneBonModelTest.php
@@ -19,6 +19,24 @@ class LigneBonModelTest extends TestCase {
         $this->assertGreaterThan(0, $id, "ID inséré doit être > 0");
      
     }
+    public function testUpdateLigneBonL() {
+    $model = new LigneBonLModel();
+
+    // Insertion initiale
+    $data = ['libelle' => 'Table basse', 'qte' => 3];
+    $id = $model->insert($data);
+
+    $updateData = ['libelle' => 'Table haute', 'qte' => 7];
+    $result = $model->update($id, $updateData);
+
+    // Récupération de la ligne mise à jour
+    $updatedLine = $model->find($id);
+
+    $this->assertTrue($result, "La méthode update doit retourner true en cas de succès");
+    $this->assertIsArray($updatedLine, "La ligne mise à jour doit être un tableau");
+    $this->assertEquals('Table haute', $updatedLine['libelle'], "Le libellé doit avoir été mis à jour");
+    $this->assertEquals(7, $updatedLine['qte'], "La quantité doit avoir été mise à jour");
+}
 
 }
 ?>
