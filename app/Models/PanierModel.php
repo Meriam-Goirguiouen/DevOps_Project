0b89f859dd151cfc8e39773c9dfc729730b30c30
Meriam-Goirguiouen <meriam.goirguiouen2002@gmail.com>
2025-05-03 18:54:48 +0100

    changes on Panier part

--- app/Models/PanierModel.php
+++ app/Models/PanierModel.php
@@ -3,8 +3,7 @@ namespace App\Models;
 
 use CodeIgniter\Model;
 
-class PanierModel extends Model
-{
+class PanierModel extends Model{
     protected $table = 'panier'; // Nom de la table dans ta base de données
     protected $primaryKey = 'id'; // Clé primaire
     protected $allowedFields = ['dateCommande', 'client']; // Champs autorisés à être insérés/modifiés
--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -2,9 +2,9 @@
 namespace App\Models;
 use CodeIgniter\Model;
 
-/*class UserModel extends Model{
+class UserModel extends Model{
     protected $table = 'user';
     protected $primaryKey = 'id';
     protected $allowedFields = ['username','email'];
 }
-?>*/
+?>
